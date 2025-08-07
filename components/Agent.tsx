bde38f9f293a207f5bbae780f0541ad16b5c1d7a
SudhanshuPandey2121 <stg271004@gmail.com>
2025-08-07 13:40:36 +0530

    updated user image

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -225,7 +225,7 @@ const Agent = ({
         <div className="card-border">
           <div className="card-content">
             <Image
-              src="/user-avatar.png"
+              src="/my-photo.jpg"
               alt="profile-image"
               width={539}
               height={539}
